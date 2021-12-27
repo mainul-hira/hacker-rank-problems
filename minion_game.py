b6d1ebb46b065e81bb4723954c2c5bd436a753b9
Mainul-Hira <mainul.hoque@upaybd.com>
2021-12-27 18:07:17 +0600

    fixed memory error issue minion_game

--- minion_game.py
+++ minion_game.py
@@ -1,26 +1,26 @@
 import re
 
-def create_combination(string):
-    res = (string[i: j] for i in range(len(string))
-            for j in range(i + 1, len(string) + 1))
-    return res
+# def create_combination(string):
+#     res = (string[i: j] for i in range(len(string))
+#             for j in range(i + 1, len(string) + 1))
+#     return res
     
 def minion_game(string):
     vowel = ('A', 'E', 'I', 'O', 'U') # Kevin = Vowel
 
-    all_words = create_combination(string)
-    used_words = set()
+    # all_words = create_combination(string)
+    # used_words = set()
     kevins_point, sturat_point = 0, 0
-
-    for i in all_words:
-        if not i in used_words and i[0] in vowel:
-            used_words.add(i)            
-            kevins_point += len(re.findall(f'(?={i})', string))
+    for idx, i in enumerate(string):
+        if i in vowel:
+            #used_words.add(i)
+            # kevins_point += len(re.findall(f'(?={i})',string))
+            kevins_point += len(string[idx:])-1
+            # print(counter[i])
             print('kev', kevins_point)
-        elif not i in used_words and i[0] not in vowel:
-            used_words.add(i)
-            sturat_point += len(re.findall(f'(?={i})', string))
-            print('stu', sturat_point)
+        else:
+            sturat_point += len(string[idx:])-1
+            # print('stu', sturat_point)
     
     if sturat_point>kevins_point:
         print(f"Stuart {sturat_point}")
